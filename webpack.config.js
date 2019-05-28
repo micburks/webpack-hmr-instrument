8b0d075c1abaf39e90870fb62c47ba93104eff0b
Mickey Burks <mburks@uber.com>
2019-05-28 14:59:21 -0700

    Add hot back to dev server, regen lock file

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const MyPlugin = require('./MyPlugin.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    hot: true,
+  },
   plugins: [
     new HtmlWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
